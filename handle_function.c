454bd6d03bc28c5e31cba0e9a8256cd343fc276c
simowardi <daoist32@gmail.com>
2023-11-15 15:25:14 -0800

    pp

--- handle_function.c
+++ handle_function.c
@@ -10,13 +10,6 @@ int char_handler(va_list args_list)
 {
 	char cc = va_arg(args_list, int);
 
-	/* Check for null character */
-	if (cc == '\0')
-	{
-		write(1, "\\0", 2);
-		return (2);
-	}
-
 	write(1, &cc, 1);
 	return (1);
 }
