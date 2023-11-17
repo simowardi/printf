22d90f6fa4fd10da4054a466d482e008ccfb1b28
simowardi <daoist32@gmail.com>
2023-11-17 06:49:59 -0800

    mm

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			if (*format == '\0' || *format == '\n')
-				_putchar(format);
+				_putchar(*format);
 				return (1);
 
 			if (*format == '%')
